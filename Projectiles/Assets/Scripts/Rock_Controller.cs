c5621ca99476ff05333b7cc43d8696bcfa1b7304
Chang Yun <boomtntc@gmail.com>
2019-02-16 14:52:05 -0800

    turrets asteroids

---
+++ Projectiles/Assets/Scripts/Rock_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock_Controller : MonoBehaviour
+{
+    public float speed;
+    float rand;
+    private Transform target;
+    float pos;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rand = Random.Range(-3f, 3f);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}

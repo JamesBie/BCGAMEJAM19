2a17e6f4014b82563dd792391bd9aea46ff92182
James Bie <jamesshifanbie@gmail.com>
2019-02-16 14:47:17 -0800

    clean up folders

---
+++ Projectiles/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public float max_speed = 3.5f;
+    public float rot_speed = 180f;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxis("Horizontal") * rot_speed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * max_speed * Time.deltaTime, 0);
+
+        pos += rot * velocity;
+
+        transform.position = pos;*/
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * max_speed);
+    
+
+        
+    }
+}

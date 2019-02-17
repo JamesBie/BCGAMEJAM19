fb174b018320be0f3719b3b6cd64b9512589f25d
hrwei2 <45253590+hrwei2@users.noreply.github.com>
2019-02-16 22:49:54 -0800

    all sounds

--- Projectiles/Assets/Scripts/AudioManager.cs
+++ Projectiles/Assets/Scripts/AudioManager.cs
@@ -25,9 +25,12 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
 
             s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+    }
+    void Start()
+    {
+        Play("Theme");
     }
     public void Play (string name)
     {
@@ -38,11 +41,8 @@ public class AudioManager : MonoBehaviour
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
-            
+   
         s.source.Play();
     }
-    void Start()
-    {
-        Play("Theme");
-    }
+    
 }
--- Projectiles/Assets/Scripts/Sound.cs
+++ Projectiles/Assets/Scripts/Sound.cs
@@ -10,8 +10,7 @@ public class Sound
     public AudioClip clip;
     [Range(0f,1f)]
     public float volume;
-    [Range(.1f,3f)]
-    public float pitch;
+
 
     public bool loop;
 

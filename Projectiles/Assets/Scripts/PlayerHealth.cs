ca36736930cae528473c402ac879be899001eb99
hoericah <hterica@DESKTOP-Q4A5O09.localdomain>
2019-02-16 21:16:32 -0800

    maybe

--- Projectiles/Assets/Scripts/PlayerHealth.cs
+++ Projectiles/Assets/Scripts/PlayerHealth.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
 
-	public int startingHealth = 100;
+	public float startingHealth = 100;
 	public Slider currentHealth;
 
-	bool isDead= false;
-	bool damaged= false;
-
-	void Awake(){
+	//bool damaged= false;
+	
+	
+	void Awake(){ //Commentating out for now bc idk what this is
 		currentHealth.value = startingHealth;
-
-
 	}
+	
 
     // Start is called before the first frame update
     void Start()
@@ -39,13 +39,16 @@ public class PlayerHealth : MonoBehaviour
         
     }
 
-
-    public void TakeDamage (int amount){
-    	damaged = true;
-    	currentHealth.value -= amount;
-    	//healthSlider.value = currentHealth;
-    	if (currentHealth.value <=0 && !isDead){
-    		isDead = true;
-    	}
+	void OnTriggerEnter(Collider other) //other is the other collider that we touch
+	{
+		//Destroy(other.gameObject); This destroys the other game object, its assets and all of its children
+		if (other.gameObject.CompareTag("Rock")){ //Tag is defined in Unity
+			Destroy(other.gameObject);//deactivate pickup object
+			currentHealth.value -= 10;
+			if (currentHealth.value <=0){
+				//Trigger Game over
+				SceneManager.LoadSceneAsync(2);
+			}
+		}
     }
 }
